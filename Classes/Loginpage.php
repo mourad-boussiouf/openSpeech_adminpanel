709e5a56ba17f88085b7134b7d8929d93028aec5
mourad-boussiouf <mourad.boussiouf@laplateforme.io>
2023-02-26 22:25:53 +0100

    ajout autoloader, test class login simple, request à l'api node à voir avec curl

---
+++ Classes/Loginpage.php
@@ -0,0 +1,46 @@
+<?php 
+class LoginPage {
+    private $apiUrl = "localhost/user/login"; //url de l'api pour login
+
+    public function __construct($apiUrl) {
+        $this->apiUrl = $apiUrl;
+    }
+
+    public function render() {
+        
+        echo '
+            <form method="post">
+                <label for="username">Nom d&rsquo;utilisateur:</label>
+                <input type="text" name="username" id="username">
+
+                <label for="password">Mot de passe:</label>
+                <input type="password" name="password" id="password">
+
+                <button type="submit" name="login">Connexion</button>
+            </form>
+        ';
+
+        if (isset($_POST['login'])) {
+
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+
+            $curl = curl_init($this->apiUrl);
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query([
+                'username' => $username,
+                'password' => $password
+            ]));
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+            $response = curl_exec($curl);
+            curl_close($curl);
+
+            if ($response === 'success') {
+                echo 'Connexion réussie';
+            } else {
+                echo 'Erreur, pas de connexion';
+            }
+        }
+    }
+}
+?>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/curl.php';
+
+use \LoginPage;
+
+$loginPage = new LoginPage('http://localhost:3000/login');
+
+$loginPage->render();
+?>
